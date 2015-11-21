e66ec627ec4f2167d13fd94ac1d24b0a890aec86
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2015-11-21 14:35:43 +0100

    add ring-json and add skeleton for api

--- src/clj/db_laboratory_diary/handler.clj
+++ src/clj/db_laboratory_diary/handler.clj
@@ -1,7 +1,12 @@
 (ns db-laboratory-diary.handler
-  (:require [compojure.core :refer [GET defroutes]]
+  (:require [compojure.core :refer [GET defroutes routes context]]
             [compojure.route :refer [not-found resources]]
-            [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+            [ring.middleware.defaults :refer [site-defaults
+                                              api-defaults
+                                              wrap-defaults]]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.util.response :refer [response]]
             [hiccup.core :refer [html]]
             [hiccup.page :refer [include-js include-css]]
             [prone.middleware :refer [wrap-exceptions]]
@@ -22,18 +27,36 @@
      [:meta {:charset "utf-8"}]
      [:meta {:name "viewport"
              :content "width=device-width, initial-scale=1"}]
+     (if (:dev env)
+       [:title "evn: DEV"])
      (include-css (if (env :dev) "css/site.css" "css/site.min.css"))]
     [:body
      mount-target
      (include-js "js/app.js")]]))
 
 
-(defroutes routes
+(defroutes site-routes
   (GET "/" [] loading-page)
   (GET "/about" [] loading-page)
-  (resources "/")
-  (not-found "Not Found"))
+  (resources "/"))
+
+(defroutes api-routes
+  (context "/api" []
+           (GET "/about" [] (response {:name "db-laboratory-diary"
+                                       :version "0.0.1"}))))
+
+(def api
+  (let [handler (wrap-defaults #'api-routes api-defaults)]
+    (if (env :dev)
+      (-> handler wrap-reload wrap-json-response wrap-exceptions wrap-reload)
+      (-> handler wrap-reload wrap-json-response))))
+
+(def site
+  (let [handler (wrap-defaults #'site-routes site-defaults)]
+    (if (env :dev) (-> handler wrap-exceptions wrap-reload) handler)))
 
 (def app
-  (let [handler (wrap-defaults #'routes site-defaults)]
-    (if (env :dev) (-> handler wrap-exceptions wrap-reload) handler)))
+  (routes
+   site
+   api
+   (not-found "Page not fund")))
