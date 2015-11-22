59bd120ce172cc42c64a82081474f0dc3f554267
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2015-11-22 14:30:01 +0100

    change migration name and add some uniques

--- resources/db/migrations/20151122005017-add-users-table.up.sql
+++ resources/db/migrations/20151122005017-add-users-table.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE users (
        id SERIAL PRIMARY KEY,
        firstname VARCHAR(100),
        lastname VARCHAR(100),
-       login VARCHAR(100) NOT NULL,
-       email VARCHAR(100),
+       login VARCHAR(100) NOT NULL UNIQUE,
+       email VARCHAR(100) NOT NULL UNIQUE,
        password VARCHAR(40) NOT NULL, -- sha1
        is_admin BOOLEAN NOT NULL DEFAULT TRUE
 );
