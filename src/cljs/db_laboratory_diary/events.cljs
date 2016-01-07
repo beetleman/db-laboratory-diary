9251b0827e91bbb35e104c4d324e159af51f65d4
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-01-08 00:38:35 +0100

    add common events handlers

---
+++ src/cljs/db_laboratory_diary/events.cljs
@@ -0,0 +1,17 @@
+(ns db-laboratory-diary.events)
+
+(defn on-add [state]
+  (fn [e]
+    (swap! state assoc :page-state :add)))
+
+(defn on-view [state]
+  (fn [e]
+    (swap! state assoc :page-state :view)))
+
+(defn on-delete [state]
+  (fn [e]
+    (swap! state assoc :page-state :delete)))
+
+(defn on-edit [state]
+  (fn [e]
+    (swap! state assoc :page-state :edit)))
