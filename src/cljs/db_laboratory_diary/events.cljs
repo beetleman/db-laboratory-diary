dc260cc9b86d8c00a363c258c336495ab59db6dc
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-01-09 01:03:20 +0100

    preventDefault in events

--- src/cljs/db_laboratory_diary/events.cljs
+++ src/cljs/db_laboratory_diary/events.cljs
@@ -2,16 +2,20 @@
 
 (defn on-add [state]
   (fn [e]
+    (.preventDefault e)
     (swap! state assoc :page-state :add)))
 
 (defn on-view [state]
   (fn [e]
+    (.preventDefault e)
     (swap! state assoc :page-state :view)))
 
 (defn on-delete [state]
   (fn [e]
+    (.preventDefault e)
     (swap! state assoc :page-state :delete)))
 
 (defn on-edit [state]
   (fn [e]
+    (.preventDefault e)
     (swap! state assoc :page-state :edit)))
