96ffb656bbdb22efa9c526939cbdacb3d561cb4a
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-01-09 01:03:58 +0100

    add funtions for build forms

--- src/cljs/db_laboratory_diary/form.cljs
+++ src/cljs/db_laboratory_diary/form.cljs
@@ -11,3 +11,27 @@
            (assoc result (keyword key) value))))
      {}
      (.getKeys form-map))))
+
+(defn form-group-input [label name {:keys [type]
+                              :or {type ""}
+                              :as params}]
+  (let [id (str "input" name)]
+    [:div.form-group
+     [:label {:for id} label]
+     [:input (merge {:class "form-control"
+                     :id id
+                     :name name
+                     :type type}
+                    params)]]))
+
+(defn form-checkbox [label name]
+  [:div.checkbox
+   [:label
+    [:input {:type "checkbox" :name name}] label]])
+
+(defn form-yes-no [cancel-event]
+  [:div {:class "btn-group btn-group-sm pull-right" :role "group"}
+    [:button {:class "btn btn-warning" :on-click cancel-event}
+    "Cancel"]
+    [:button {:class "btn btn-success" :type "submit"}
+     "Ok"]])
