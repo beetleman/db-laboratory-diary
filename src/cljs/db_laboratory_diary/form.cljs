785639853e1bed437d2b6ed27f881ec53c0d04bd
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-02-13 11:12:38 +0100

    add `on-add-fn`

--- src/cljs/db_laboratory_diary/form.cljs
+++ src/cljs/db_laboratory_diary/form.cljs
@@ -1,5 +1,7 @@
 (ns db-laboratory-diary.form
-  (:require [goog.dom.forms :as gforms]))
+  (:require [goog.dom.forms :as gforms]
+            [db-laboratory-diary.api :as api]
+            [accountant.core :as accountant]))
 
 (defn form->map [form]
   (let [form-map (gforms/getFormDataMap form)]
@@ -36,9 +38,25 @@
    [:label
     [:input {:type "checkbox" :name name}] label]])
 
+
 (defn form-yes-no [cancel-event]
   [:div {:class "btn-group btn-group-sm pull-right" :role "group"}
     [:button {:class "btn btn-warning" :on-click cancel-event}
     "Cancel"]
     [:button {:class "btn btn-success" :type "submit"}
      "Ok"]])
+
+
+(defn on-add-fn [state state-target api-url redirect-url add-msg add-fn]
+  (fn [event]
+    (.preventDefault event)
+    (let [params (add-fn (form->map (.-target event)))]
+      (api/api-post
+       state state-target api-url params
+       {:handler-fn (fn [a k v]
+                      (if-let [error (:error v)]
+                        (assoc a :danger-msg error)
+                        (do
+                          (accountant/navigate! redirect-url)
+                          (assoc a :info-msg
+                                 add-msg))))}))))
--- src/cljs/db_laboratory_diary/users.cljs
+++ src/cljs/db_laboratory_diary/users.cljs
@@ -2,9 +2,7 @@
   (:require [db-laboratory-diary.tables :as tables]
             [db-laboratory-diary.events :as e]
             [db-laboratory-diary.form :as form]
-            [db-laboratory-diary.api :as api]
             [reagent-forms.core :refer [bind-fields]]
-            [accountant.core :as accountant]
             [reagent.core :as r]))
 
 (defn users-table [state]
@@ -20,23 +18,16 @@
      :email "Email"
      :is_admin "Admin"}]])
 
-(defn on-add-user [state event]
-  (.preventDefault event)
-  (let [user (form/form->map (.-target event))
-        user (if (:is_admin user) (assoc user :is_admin true)
-                 (assoc user :is_admin false))]
-    (api/api-post
-     state :user "users" user
-     {:handler-fn (fn [a k v]
-                    (if-let [error (:error v)]
-                      (assoc a :danger-msg error)
-                      (do
-                        (accountant/navigate! "/users")
-                        (assoc a :info-msg
-                               "User added"))))})))
+
+(defn on-add-user-fn [state]
+  (form/on-add-fn state :user "users" "/users" "User added"
+                  (fn [user] (if (:is_admin user)
+                               (assoc user :is_admin true)
+                               (assoc user :is_admin false)))))
+
 
 (defn add-user-form [state]
-  [:form {:on-submit (partial on-add-user state)}
+  [:form {:on-submit (on-add-user-fn state)}
    [form/form-group-input "Username" "username"]
    [form/form-group-input "Email" "email" {:type "email"}]
    [form/form-checkbox "Admin" "is_admin"]
