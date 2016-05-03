093c0543a7feea8cafa20012e67b4a449d37558b
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-05-03 16:02:58 +0200

    upgrade cider-nrepl

--- project.clj
+++ project.clj
@@ -78,7 +78,7 @@
 
                    :source-paths ["env/dev/clj"]
                    :plugins [[lein-figwheel "0.5.0-SNAPSHOT"]
-                             [cider/cider-nrepl "0.12.0-SNAPSHOT"]
+                             [cider/cider-nrepl "0.13.0-SNAPSHOT"]
                              [refactor-nrepl "2.0.0-SNAPSHOT"]
                              [lein-less "1.7.5"]]
 
